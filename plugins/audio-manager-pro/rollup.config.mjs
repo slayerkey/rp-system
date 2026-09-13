0106c232e14a240b832edbfa1eec8fdc3ac98257
slayerkey <34904637+slayerkey@users.noreply.github.com>
2026-09-12 22:05:06 -0700

    audio manager: add plugin bundler

---
+++ plugins/audio-manager-pro/rollup.config.mjs
@@ -0,0 +1,17 @@
+import commonjs from "@rollup/plugin-commonjs";
+import nodeResolve from "@rollup/plugin-node-resolve";
+import terser from "@rollup/plugin-terser";
+
+export default {
+  input: "src/plugin.js",
+  output: {
+    file: "com.packrat.audio-manager-pro.sdPlugin/bin/plugin.js",
+    format: "es",
+    sourcemap: false,
+  },
+  plugins: [
+    nodeResolve({ browser: false, exportConditions: ["node"], preferBuiltins: true }),
+    commonjs(),
+    terser(),
+  ],
+};
